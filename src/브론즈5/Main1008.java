64c44a95cf1ee64221591b2e75f2c8739d79207d
kk0506014 <kk0506014@gmail.com>
2025-12-12 09:44:14 +0900

    25.12.12 - 1008

--- src/브론즈5/Main1008.java
+++ src/브론즈5/Main1008.java
@@ -1,6 +1,8 @@
+package 브론즈5;
+
 import java.util.Scanner;
 
-public class Main {
+public class Main1008 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
