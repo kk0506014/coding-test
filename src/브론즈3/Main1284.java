749c47513c9c85c5bf697ee37c4d7cb945887f94
kk0506014 <kk0506014@gmail.com>
2025-12-12 10:51:00 +0900

    25.12.12 - 1284

--- src/브론즈3/Main1284.java
+++ src/브론즈3/Main1284.java
@@ -1,6 +1,8 @@
+package 브론즈3;
+
 import java.util.Scanner;
 
-public class Main {
+public class Main1284 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
